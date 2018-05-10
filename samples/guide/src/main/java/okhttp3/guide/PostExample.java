1bc71dc5d06513323799ac0174378d7481da846b
lanboys1225 <lanboys1225@gmail.com>
2018-05-10 17:38:41 +0800

    modify postExample

--- samples/guide/src/main/java/okhttp3/guide/PostExample.java
+++ samples/guide/src/main/java/okhttp3/guide/PostExample.java
@@ -1,6 +1,8 @@
 package okhttp3.guide;
 
 import java.io.IOException;
+
+import okhttp3.FormBody;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -13,7 +15,29 @@ public class PostExample {
 
   OkHttpClient client = new OkHttpClient();
 
+  String postForm(String url, String player1, String player2) throws IOException {
+
+    //https://blog.csdn.net/muyi_amen/article/details/58586605
+
+    //Form表单格式的参数传递
+    FormBody formBody = new FormBody
+            .Builder()
+            .add("player1",player1)
+            .add("player2",player2)
+            .build();
+
+    Request request = new Request.Builder()
+        .url(url)
+        .post(formBody)
+        .build();
+    try (Response response = client.newCall(request).execute()) {
+      return response.body().string();
+    }
+  }
+
   String post(String url, String json) throws IOException {
+
+    //创建RequestBody对象，将参数按照指定的MediaType封装
     RequestBody body = RequestBody.create(JSON, json);
     Request request = new Request.Builder()
         .url(url)
@@ -25,6 +49,15 @@ public class PostExample {
   }
 
   String bowlingJson(String player1, String player2) {
+
+    //也可以使用JSONObject封装参数
+    //JSONObject jsonObject = new JSONObject();
+    //try {
+    //  jsonObject.put("参数名","参数值");
+    //} catch (JSONException e) {
+    //  e.printStackTrace();
+    //}
+
     return "{'winCondition':'HIGH_SCORE',"
         + "'name':'Bowling',"
         + "'round':4,"
@@ -38,8 +71,11 @@ public class PostExample {
 
   public static void main(String[] args) throws IOException {
     PostExample example = new PostExample();
-    String json = example.bowlingJson("Jesse", "Jake");
-    String response = example.post("http://www.roundsapp.com/post", json);
+    //String json = example.bowlingJson("Jesse", "Jake");
+    //String response = example.post("http://www.roundsapp.com/post", json);
+
+    String response = example.postForm("http://www.roundsapp.com/post","Jesse", "Jake");
+
     System.out.println(response);
   }
 }
